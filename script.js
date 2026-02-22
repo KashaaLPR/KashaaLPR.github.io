9aabb04552a887c1e804a63d0d6c0f1c1c75e86d
KashaaLPR <rerampedan16@gmail.com>
2026-02-22 17:05:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ function sapa() {
     alert("Halo! Selamat datang di website saya 😄");
 }
 function favorit() {
-    alert("Saya sangat menyukai pelajaran matematika, IPA, desain, dan pemrograman. Di matematika saya sangat menyukainya ketika mengetahui suatu rumus yang menjadi berguna untuk kejadian nyata. Untuk IPA, saya sangat menyukai ketika membahas anatomi semua makhluk hidup. Di desain, saya sangat menyukai ketika di COREL dan Animasi. Untuk pemrograman saya menyukai semuanya.");    
+    document.getElementById("hasil").innerHTML =
+        "Saya sangat menyukai pelajaran matematika, IPA, desain, dan pemrograman. Di matematika saya sangat menyukainya ketika mengetahui suatu rumus yang menjadi berguna untuk kejadian nyata. Untuk IPA, saya sangat menyukai ketika membahas anatomi semua makhluk hidup. Di desain, saya sangat menyukai ketika di COREL dan Animasi. Untuk pemrograman saya menyukai semuanya.";    
 }
 function game() {
-    alert("Saya remaja, sudah pasti suka bermain game. Remaja manasih yang tidak suka bermain game? Game Android saya menyukai Mobile Legend, Free Fire, Honor Of Kings. Game PC saya menyukai Valorant. Dan mungkin seterusnya akan lebih banyak lagi game yang saya sukai.");
+    document.getElementById("hasil").innerHTML =
+        "Saya remaja, sudah pasti suka bermain game. Remaja manasih yang tidak suka bermain game? Game Android saya menyukai Mobile Legend, Free Fire, Honor Of Kings. Game PC saya menyukai Valorant. Dan mungkin seterusnya akan lebih banyak lagi game yang saya sukai.";
 }
 function pelajari() {
-    alert("Saya juga sangat suka bermain game. Reamaja manasih yang tidak suka bermain game? Game Android saya menyukai Mobile Legend, Free Fire, Honor Of Kings. Game PC saya menyukai Valorant. Dan mungkin seterusnya akan lebih banyak lagi game yang saya sukai.");
+    document.getElementById("hasil").innerHTML =
+        "Saya juga sangat suka bermain game. Reamaja manasih yang tidak suka bermain game? Game Android saya menyukai Mobile Legend, Free Fire, Honor Of Kings. Game PC saya menyukai Valorant. Dan mungkin seterusnya akan lebih banyak lagi game yang saya sukai.";
 }
